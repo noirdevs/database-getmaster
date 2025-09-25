21d704fa4717183ac2f29bad7b82ab9f9d071286
Rlxhnoir <roni09.ramdani11@gmail.com>
2025-09-25 13:55:54 +0700

    Tambah logika untuk menyimpan output ke file TXT
    Skrip prosesor sekarang akan memformat hasil query database menjadi
    sebuah string yang dipisahkan oleh pipe (|) dan menyimpannya ke dalam
    file .txt.
    
    Nama file dibuat secara dinamis berdasarkan tanggal data yang diambil.
    Menggunakan `file_put_contents()` untuk penulisan file yang efisien.

--- get_data.php
+++ get_data.php
@@ -1,37 +1,52 @@
 <?php
 
+$date = date('Y-m-d', strtotime("-1 days")); 
+$path = "/home/datawp";
+$filename = $date . ".txt";
 $config = require_once 'config.php';
 require_once 'DatabaseManager.php';
 
 DatabaseManager::setConfig($config);
 
-echo "Memulai proses...\n";
+echo "Memulai proses pengambilan data untuk tanggal" . $date ."\n";
 
 try {
     $db_name = 'mysql';
-    $pdo_mysql = DatabaseManager::getConnection($db_name);
+    $pdo = DatabaseManager::getConnection($db_name);
 
     $tableName = $config['connections'][$db_name]['table'];
-    $date = date('Y-m-d', strtotime("-1 days")); 
 
     echo "Mengambil data dari tabel '{$tableName}' untuk tanggal {$date}...\n";
 
-    $sql = "SELECT transaksi_id, tanggal_transaksi, subtotal, service(jikaada), tax FROM {$tableName} WHERE tanggal_transaksi LIKE '{$date}%'";
+    $sql = "SELECT transaksi_id, tanggal_transaksi, subtotal, service(jikaada), tax FROM {$tableName} WHERE tanggal_transaksi LIKE ?";
     
-    $stmt = $pdo_mysql->prepare($sql);
+    $stmt = $pdo->prepare($sql);
     $stmt->execute(['%' . $date . '%']);
 
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     if ($results) {
-        echo "Data berhasil ditemukan: " . count($results) . " baris.\n";
-        print_r($results[0]); 
+        echo "Terdapat " . count($results) . " struk.\n";
+
+	$string = '';
+
+	foreach ($results as $struk) {
+		$baris .= implode('|', $struk) . "\n";
+	}
+        echo "Data akan disimpan di " . $path . "/NEW/" . $filename ."\n";
+
+	if (!is_dir(dirname($pathLengkap))) {
+            mkdir(dirname($pathLengkap), 0755, true);
+        }
+
+	file_put_contents($pathLengkap, $string_output);
+
     } else {
-        echo "Tidak ada data untuk tanggal tersebut.\n";
+        echo $date . "Tidak ada data"."\n";
     }
 
 
 } catch (Exception $e) {
-    echo "\n!!! TERJADI ERROR !!!\n";
+    echo "\n!!! ERROR GAISSS !!!\n";
     echo "Pesan: " . $e->getMessage() . "\n";
 }
